e443c6a0dbe02bd9fc7f2ece0b9c1b08121f594b
Paul Baluyot <zero4732@yahoo.com>
2018-11-27 05:56:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function deleteFromObjectByKey(object, key) {
   var boaz = Object.assign({}, object);
+<<<<<<< HEAD
   delete boaz[key];
   return boaz;
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
+=======
+  return boaz;
+>>>>>>> aed63bf7bc674fbba7e928cf1dfeb9bbcc7e38a7
 }
